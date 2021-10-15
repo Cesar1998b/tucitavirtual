a92674f5bcc044e2d56e48daf6ea49b08a96d311
Cesar David Botina <cesarbotina98@gmail.com>
2021-10-15 17:21:11 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from '@home/home.component';
+import { ProductoModule } from '@producto/producto.module';
+import { CoreModule } from '@core/core.module';
+import { CookieService } from 'ngx-cookie-service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductoModule,
+    CoreModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  providers: [CookieService],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
