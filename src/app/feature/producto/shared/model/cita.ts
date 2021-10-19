c8a9f31829446913e146292a2b2fd6cd3b71083a
Cesar David Botina <cesarbotina98@gmail.com>
2021-10-19 14:56:51 -0500

    delete constructor class

--- src/app/feature/producto/shared/model/cita.ts
+++ src/app/feature/producto/shared/model/cita.ts
@@ -7,15 +7,4 @@ export class Cita {
     cityUser: string;
     date: string;
     tarifa: number;
-
-  constructor(id: number, nameUser: string, lastNameUser: string, emailUser: string, telUser: number, cityUser: string, date: string, tarifa: number) {
-      this.id = id;
-      this.nameUser = nameUser;
-      this.lastNameUser = lastNameUser;
-      this.emailUser = emailUser;
-      this.telUser = telUser;
-      this.cityUser = cityUser;
-      this.date = date;
-      this.tarifa = tarifa;
-  }
 }
