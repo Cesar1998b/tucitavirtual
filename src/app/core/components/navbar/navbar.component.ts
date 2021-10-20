7d3b4fe320415b90e2b8f835662fb0480de3df9d
Cesar David Botina <cesarbotina98@gmail.com>
2021-10-20 09:54:18 -0500

    changes in nav to sidenav for responsive design

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,39 +1,55 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MenuItem } from '@core/modelo/menu-item';
+import { Component, OnInit, Input } from "@angular/core";
+import { MenuItem } from "@core/modelo/menu-item";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: 'navbar.component.html',
-  styles: [`:host {
-    border: 0 solid #e1e1e1;
-    display: block;
-    height: 48px;
-    padding: 0 16px;
-  }
+  selector: "app-navbar",
+  templateUrl: "navbar.component.html",
+  styles: [
+    `
+      span a {
+        font-size: 20px;
+      }
 
-  nav a {
-    color: #fff;
-    font-size: 16px;
-    font-weight: 500;
-    line-height: 48px;
-    margin-right: 20px;
-    text-decoration: none;
-    vertical-align: middle;
-    cursor: pointer;
-  }
+      span a mat-icon{
+        font-size: 18px;
+        padding-left: 2px;
+      }
 
-  nav a.router-link-active {
-    color: #106cc8;
-  }`],
+      .nav {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        box-sizing: border-box;
+        margin-left: 15px;
+      }
+
+      a {
+        color: #fff;
+        font-size: 16px;
+        font-weight: 500;
+        line-height: 48px;
+        margin-right: 20px;
+        text-decoration: none;
+        vertical-align: middle;
+        cursor: pointer;
+      }
+
+      a.router-link-active {
+        color: #106cc8;
+      }
+
+      .menu-spacer {
+        flex: 1 1 auto;
+      }
+    `,
+  ],
 })
 export class NavbarComponent implements OnInit {
-
   @Input()
   items: MenuItem[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
