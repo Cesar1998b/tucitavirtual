c4813611cc8a594a2fd08f48ee0c4f8bfcadb2af
Cesar David Botina <cesarbotina98@gmail.com>
2021-10-20 10:51:39 -0500

    create header component and  delete toolbar-navbar components

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MenuItem } from "@core/modelo/menu-item";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  @Input()
+  items: MenuItem[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
